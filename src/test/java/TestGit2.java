43db57ccfbf13a2c9a71d47ede43464bb676754a
Angelina Morgatskaya <morgatskaya.angelina@gmail.com>
2025-04-12 00:22:54 +0300

    init commit

---
+++ src/test/java/TestGit2.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestGit2 {
+    @Test
+    void successfulSearchTest1() {
+        open("https://ya.ru/");
+        $("[name=text]").setValue("selenide").pressEnter();
+        $("[id=search-result]").shouldHave(text("ru.selenide.org"));
+    }
+    @Test
+    void successfulSearchTest2() {
+        open("https://www.bing.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=b_results]").shouldHave(text("https://selenide.org"));
+    }
+}
