a29086fdb996a690dd78f74a6c9432582211099f
Bricran <108898629+Bricran@users.noreply.github.com>
2025-04-12 00:47:49 +0300

    Update TestGit2.java

--- src/test/java/TestGit2.java
+++ src/test/java/TestGit2.java
@@ -8,13 +8,13 @@ public class TestGit2 {
     @Test
     void successfulSearchTest1() {
         open("https://ya.ru/");
-        $("[name=11111text]").setValue("selenide").pressEnter();
+        $("[name=text]").setValue("selenide").pressEnter();
         $("[id=search-result]").shouldHave(text("ru.selenide.org"));
     }
     @Test
     void successfulSearchTest2() {
         open("https://www.bing.com/");
-        $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=b_results]").shouldHave(text("https://selenide.org"));
+        $("[name=1q]").setValue("selenide").pressEnter();
+        $("[id=1b_results]").shouldHave(text("https://selenide.org"));
     }
 }
